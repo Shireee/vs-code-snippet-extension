4eb3c31e2c2703896b75d3313483e13d03c76b0b
Bagdeev Vladimir <kirog11az@gmail.com>
2025-01-28 13:10:44 +0300

    Add index.ts generation feature

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# Explanation
+# Extension overview
+
+Extension provide two features:
+
+1. Generate `index.ts` file
+2. Snippets for react/ts/tanstack-query
+
+## Generate index.ts
+
+This feature generate `index.ts` in same directoryt with export statement based on filename of activetab.
+
+<u>e.g</u>: we are on `MyComponent.tsx` tab, and run feature. `index.ts` will be:
+
+```
+export { MyComponent } from './MyComponent';
+```
+
+Run:
+
+1. Press `ctrl+shift+\` - keybinding
+2. Run `shireee.generateIndex.ts` - Command Palette
+
+## Snippets
 
 ### `uss` - useState snippet
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,2 +1,32 @@
-// Input file to compile
-// There are only snippets in snippets.json
+import * as vscode from "vscode";
+import * as path from "path";
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand("shireee.generateIndex.ts", () => {
+    // check if we have open editor tab
+    if (vscode.window.activeTextEditor) {
+      const activeFileUri = vscode.window.activeTextEditor.document.fileName;
+      const indexFileUri = vscode.Uri.joinPath(
+        vscode.Uri.file(path.dirname(activeFileUri)),
+        "index.ts"
+      );
+
+      const fileName = path.basename(activeFileUri, path.extname(activeFileUri));
+      const content = new TextEncoder().encode(`export { ${fileName} } from './${fileName}';\n`);
+
+      // Writing index.ts
+      try {
+        vscode.workspace.fs.writeFile(indexFileUri, content);
+        vscode.window.showInformationMessage(`index.ts generated`);
+      } catch (error) {
+        vscode.window.showErrorMessage(`Failed generated index.ts`);
+      }
+    } else {
+      vscode.window.showInformationMessage("No active editor");
+    }
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
